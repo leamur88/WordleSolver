0cf0f16d0ab0cd7e90ac6cc5275ac6a347540ec6
Jonathan Leibovich <leibo023@umn.edu>
2022-01-26 11:31:46 -0600

    refactored elimination function and split it into 3 different functions

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 validLetters = []
-startWord = "irate"
+# startWord = "irate"
+# startWord = "audio"
+startWord = "penis"
 words = {}
 wordsleft = -1
 wordProbs = {'a': .078, 'b': .02, 'c': .04, 'd': .038, 'e': .11, 'f': .014, 'g': .03, 'h': .023, 'i': .082, 'j': .0021,
@@ -19,7 +21,8 @@ wordProbs = {'a': .078, 'b': .02, 'c': .04, 'd': .038, 'e': .11, 'f': .014, 'g':
 
 
 def init():
-    with open('supposedWords.csv', encoding='utf-8-sig') as f:
+    # with open('supposedWords.csv', encoding='utf-8-sig') as f:
+    with open('validWords.csv') as f:
         csvreader = csv.reader(f)
         validWords = []
         for i in csvreader:
@@ -40,6 +43,10 @@ def playGame():
     webpage.send_keys(wordChosen)
     webpage.send_keys(Keys.RETURN)
     for i in range(0, 6):
+        removeChar = []
+        removePos = []
+        removeNotIn = []
+        removeMultiple = []
 
         javascript = """return document
         .querySelector('game-app').shadowRoot
@@ -53,32 +60,40 @@ def playGame():
         """.format(i)
 
         board = driver.execute_script(javascript)
-        print(len(board))
-        remove = []
-        removeMultiple = []
         correct = 0
         for j in range(5):
             evaluation = board[j].get_attribute("evaluation")
             print(j, board[j].get_attribute("letter"), evaluation)
             if evaluation == "absent":
                 if wordChosen[j] not in validLetters:
-                    remove.append([wordChosen[j], False, -1])
+                    removeChar.append(wordChosen[j])
                 else:
                     removeMultiple.append(wordChosen[j])
             elif evaluation == "present":
                 validLetters.append(wordChosen[j])
-                remove.append([wordChosen[j], True, -1])
+                removeNotIn.append(wordChosen[j])
             else:
-                correct +=1
+                correct += 1
                 validLetters.append(wordChosen[j])
-                remove.append([wordChosen[j], True, j])
+                removePos.append([wordChosen[j], j])
         words.pop(wordChosen)
+
         if correct == 5:
             return i
-        for k in remove:
-            removeLetter(k[0], k[1], k[2])
-        for l in removeMultiple:
-            removeMultipleLetter(l)
+
+        for c in removeChar:
+            if c not in validLetters:
+                removeLetter(c)
+
+        for p in removePos:
+            removeSpecificPosition(p[0], p[1])
+
+        for n in removeNotIn:
+            removeLetterNotInWord(n)
+
+        for m in removeMultiple:
+            removeMultipleLetter(m)
+
         time.sleep(5)
         wordsleft = len(words.keys())
         print("We have " + str(wordsleft) + " words left to guess!")
@@ -99,25 +114,39 @@ def playGame():
 def removeMultipleLetter(letter):
     remove = []
     for word in words.keys():
-        if word.count(letter) >=2:
+        if word.count(letter) >= 2:
             remove.append(word)
     for i in remove:
         words.pop(i)
-def removeLetter(letter, inWord, pos=-1):
+
+def removeSpecificPosition(letter, pos):
     remove = []
-    if inWord:
-        if pos > -1:
-            for word in words.keys():
-                if word[pos] != letter:
-                    remove.append(word)
-        else:
-            for word in words.keys():
-                if letter not in word:
-                    remove.append(word)
-    else:
-        for word in words.keys():
-            if letter in word:
-                remove.append(word)
+    for word in words.keys():
+        if word[pos] != letter:
+            # if word == "whack":
+            #     print("hello?")
+            remove.append(word)
+    for i in remove:
+        words.pop(i)
+
+
+def removeLetterNotInWord(letter):
+    remove = []
+    for word in words.keys():
+        if letter not in word:
+            # if word == "whack":
+            #     print("hello??")
+            remove.append(word)
+    for i in remove:
+        words.pop(i)
+
+def removeLetter(letter):
+    remove = []
+    for word in words.keys():
+        if letter in word:
+            # if word == "whack":
+            #     print("hello???")
+            remove.append(word)
     for i in remove:
         words.pop(i)
 
@@ -143,6 +172,6 @@ if __name__ == '__main__':
     print(len(words.keys()))
     # print(chooseWord())
     num = playGame()
-    print("Congratualtions!!!! you made it out in " + str(num + 1) + "moves.")
+    print("Congratualtions!!!! you made it out in " + str(num + 1) + " moves.")
     # for i in range(6):
     # guess = input("What would you like your guess to be? ")
