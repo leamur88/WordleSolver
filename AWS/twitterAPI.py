7f77a5dcfa400248379c7124b488e93089b6f89e
Jonathan Leibovich <leibo023@umn.edu>
2022-01-28 23:53:00 -0600

    done with this for now

--- AWS/twitterAPI.py
+++ AWS/twitterAPI.py
@@ -1,9 +1,12 @@
 import json
 import boto3
 from requests_oauthlib import OAuth1Session
+from datetime import date
 
 s3 = boto3.client('s3')
 bucket = 'wordlesolvertwitter'
+
+
 def postTweet(consumer_token, consumer_key, access_token, access_token_secret, payload):
     oauth = OAuth1Session(
         consumer_token,
@@ -26,6 +29,7 @@ def postTweet(consumer_token, consumer_key, access_token, access_token_secret, p
     # Saving the response as JSON
     return response.json()
 
+
 def createPayload(msg, reply):
     if reply != 0:
         return {
@@ -33,9 +37,10 @@ def createPayload(msg, reply):
             "reply": {
                 "in_reply_to_tweet_id": reply
             }
-                }
+        }
     else:
         return {"text": msg}
+
 
 def createMessage(fname, wordleday, message):
     if fname == "supposedWords.csv":
@@ -47,30 +52,20 @@ def createMessage(fname, wordleday, message):
         msg += "\n\n" + message
         return msg
 
+
 def sendTweet(message, fname):
     secretsFile = s3.get_object(Bucket=bucket, Key='secretsWordle.json')
     secretsContent = secretsFile['Body']
 
     secrets = json.loads(secretsContent.read())
-    f = open('results.json', 'r+')
-    data = json.load(f)
-    replyID = 0
-    wordleDay = 0
-    if fname == 'validWords.csv':
-        data['wordleDay'] += 1
-    else:
-        replyID = data["replyID"]
 
-    f.seek(0)
-    json.dump(data, f, indent=4)
-    f.truncate()
-    f.close()
+    replyID = 0
+    wordleDay = 223 + (date.today() - date(2022,1,28)).days
+
     ct = secrets['consumer_token']
     ck = secrets['consumer_key']
     at = secrets['access_token']
     ats = secrets['access_key']
-
-
 
     msg = createMessage(fname, wordleDay, message)
 
