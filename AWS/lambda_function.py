21acb862491b65ccb4998c05a0932d65449ebbf5
Jonathan Leibovich <leibo023@umn.edu>
2022-01-28 19:23:15 -0600

    updated tweet

--- AWS/lambda_function.py
+++ AWS/lambda_function.py
@@ -2,10 +2,11 @@ from wordleSolver import init, playGame
 from twitterAPI import sendTweet
 
 def lambda_handler(event, context):
-    word = init('supposedWords.csv')
+    filename = 'supposedWords.csv'
+    word = init(filename)
     response = playGame(word)
     print("Congratulations!!!! you made it out in " + str(response[0] + 1) + " moves.")
-    reply_id = sendTweet(response[1], 0)
+    reply_id = sendTweet(response[1], filename)
     print("tweet\n", response[1])
     return True
 
--- AWS/twitterAPI.py
+++ AWS/twitterAPI.py
@@ -37,18 +37,36 @@ def createPayload(msg, reply):
     else:
         return {"text": msg}
 
-def sendTweet(msg, replyID):
-    key = 'secretsWordle.json'
-    response = s3.get_object(Bucket=bucket, Key=key)
-    content = response['Body']
+def createMessage(fname, wordleday, message):
+    if fname == "supposedWords.csv":
+        msg = "Smart Robot, Wordle Day" + str(wordleday)
+        msg += "\n" + message
+        return msg
+    else:
+        msg = "Not as Smart Robot, Wordle Day" + str(wordleday)
+        msg += "\n" + message
+        return msg
 
-    secrets = json.loads(content.read())
+def sendTweet(message, fname):
+    secretsFile = s3.get_object(Bucket=bucket, Key='secretsWordle.json')
+    resultsFile = s3.get_object(Bucket=bucket, Key='results.json')
+    secretsContent = secretsFile['Body']
+    resultsContent = resultsFile['Body']
+
+    secrets = json.loads(secretsContent.read())
+    results = json.loads(resultsContent.read())
+
     ct = secrets['consumer_token']
     ck = secrets['consumer_key']
     at = secrets['access_token']
     ats = secrets['access_key']
 
-    payload = createPayload(msg_to_send, reply_id)
+    replyID = results['replyID']
+    wordleDay = results['wordleDay']
+
+    msg = createMessage(fname, wordleDay, message)
+
+    payload = createPayload(msg, replyID)
     partial_response = postTweet(ct, ck, at, ats, payload)
     reply_id = partial_response['data']['id']
     return reply_id
